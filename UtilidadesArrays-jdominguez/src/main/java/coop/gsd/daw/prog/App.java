86a2aecc9a5429f069c0322ffd9c76db2412876d
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2023-12-13 11:04:18 +0100

    añadido mensaje de bienvenida

--- UtilidadesArrays-jdominguez/src/main/java/coop/gsd/daw/prog/App.java
+++ UtilidadesArrays-jdominguez/src/main/java/coop/gsd/daw/prog/App.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class App {
 
 	public static void main(String[] args) {
+		System.out.println("hola Buenas");
 		int[] array = {1,2,3,4,5,6};
 		System.out.println(Arrays.toString(array));
 		UtilidadesArrays.invertirArray(array);
