5f3daa17b56283c92f0330be1f5b93187a6d789e
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-22 10:45:25 +0100

    cambios en imagenfil

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
@@ -2,6 +2,6 @@ package com.gsd.daw.prog;
 public class App {
 
 	public static void main(String[] args) {
-		
+		ImagenFil.ConversorArray()
 		} 
 	}
--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
@@ -1,8 +1,24 @@
 package com.gsd.daw.prog;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ImagenFil {
 
-	public static String[] ConversorArray() {
+	public static String[] ConversorArray(String[] fil) {
+		Scanner inputFile = null;
+		try {
+			File f = new File(fil[0]);
+			inputFile = new Scanner( f );
+
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: no se puede abrir el fichero [" + fil[0] + "]");
+		}
+		while (inputFile.hasNext()) {
+            String line = inputFile.nextLine();
+            System.out.println("---" + line + "---");
+        }
 		return null;
 	}
 }
