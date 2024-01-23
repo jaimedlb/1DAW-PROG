fcc0bb42dd25dc226aa95e5553665010c776fe0a
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-23 12:21:11 +0100

    Splitear array

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
@@ -3,7 +3,16 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		 String[] h=ImagenFil.ConversorArray(args);
-		ImagenFil.StringconversorFil(h);
+		 String[] imagen=ImagenFil.ConversorArray(args);
+		 for (int i = 2; i < imagen.length-3; i++) {
+			String[] numerosSpliteados=imagen[i].split(",");
+			int[] numerosSpliteadosInt=new int[3];
+			for (int j = 0; j < numerosSpliteados.length; j++) {
+				numerosSpliteadosInt[j]=Integer.parseInt(numerosSpliteados[j]);
+			}
+			
+		}
+		ImagenFil.StringconversorFil(imagen);
+		
 		} 
 	}
