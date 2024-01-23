f94357bd1dded3fdd83d04a38836c6ba0a88ac19
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-23 12:25:54 +0100

    suma de brillo

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
@@ -1,18 +1,29 @@
 package com.gsd.daw.prog;
+
 public class App {
 
 	public static void main(String[] args) {
-		
-		 String[] imagen=ImagenFil.ConversorArray(args);
-		 for (int i = 2; i < imagen.length-3; i++) {
-			String[] numerosSpliteados=imagen[i].split(",");
-			int[] numerosSpliteadosInt=new int[3];
+
+		String[] imagen = ImagenFil.ConversorArray(args);
+		for (int i = 2; i < imagen.length - 3; i++) {
+			String[] numerosSpliteados = imagen[i].split(",");
+			int[] numerosSpliteadosInt = new int[3];
 			for (int j = 0; j < numerosSpliteados.length; j++) {
-				numerosSpliteadosInt[j]=Integer.parseInt(numerosSpliteados[j]);
+				numerosSpliteadosInt[j] = Integer.parseInt(numerosSpliteados[j]);
+				numerosSpliteadosInt[j] = numerosSpliteadosInt[j] + Integer.parseInt(args[1]);
+				if (numerosSpliteadosInt[j] < 0) {
+					numerosSpliteadosInt[j] = 0;
+				}
+				if (numerosSpliteadosInt[j] > 255) {
+					numerosSpliteadosInt[j] = 255;
+				}
 			}
+
+			
+			
 			
 		}
 		ImagenFil.StringconversorFil(imagen);
-		
-		} 
+
 	}
+}
