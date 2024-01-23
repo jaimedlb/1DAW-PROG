76a6358cd05b470ba9fe70b2301d39f9484d78e9
jaime <jaime>
2024-01-23 16:18:09 +0100

    inputfile close

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
@@ -16,46 +16,47 @@ public class ImagenFil {
 			System.err.println("ERROR: no se puede abrir el fichero [" + fil[0] + "]");
 			return null;
 		}
-		String comentario=String.valueOf(inputFile.nextLine());
+		String comentario = String.valueOf(inputFile.nextLine());
 		int filas = Integer.parseInt(inputFile.nextLine());
 		int columnas = Integer.parseInt(inputFile.nextLine());
-		String[] parametros = new String[(filas * columnas)+3];
-		parametros[0]=comentario;
-		parametros[1]=String.valueOf(filas);
-		parametros[2]=String.valueOf(columnas);
+		String[] parametros = new String[(filas * columnas) + 3];
+		parametros[0] = comentario;
+		parametros[1] = String.valueOf(filas);
+		parametros[2] = String.valueOf(columnas);
 
 		for (int i = 3; i < parametros.length; i++) {
 			String linea = inputFile.nextLine();
 			parametros[i] = linea;
 
 		}
-
+		inputFile.close();
 		return parametros;
 	}
-	/*public static boolean StringconversorFil(String[] fil) {
-		
-		try {
-		String[] nombreSpliteado=String.valueOf(fil[0]).split("#|\\.");
-		String nombreArchivo=nombreSpliteado[1];
-		String nombreFinal=nombreArchivo+"-cambiado.fil";
-		File archivo =new File(nombreFinal);
-			 
-			FileWriter escribo = new FileWriter(archivo);
-			escribo.write("#"+nombreFinal+"\n");
-			for (int i = 1; i < fil.length; i++) {
-				if(i==fil.length-1) {
-					escribo.write(fil[i]);
-				}else {
-					escribo.write(fil[i]+"\n");					
-				}
-			}
-			escribo.close();
-		}catch (IOException e) {
-			System.out.println("Ha ocurrido un error al crear el archivo.");
-            e.printStackTrace();
-            return false;
-		}
-		return true;
-	}*/
+	/*
+	 * public static boolean StringconversorFil(String[] fil) {
+	 * 
+	 * try {
+	 * String[] nombreSpliteado=String.valueOf(fil[0]).split("#|\\.");
+	 * String nombreArchivo=nombreSpliteado[1];
+	 * String nombreFinal=nombreArchivo+"-cambiado.fil";
+	 * File archivo =new File(nombreFinal);
+	 * 
+	 * FileWriter escribo = new FileWriter(archivo);
+	 * escribo.write("#"+nombreFinal+"\n");
+	 * for (int i = 1; i < fil.length; i++) {
+	 * if(i==fil.length-1) {
+	 * escribo.write(fil[i]);
+	 * }else {
+	 * escribo.write(fil[i]+"\n");
+	 * }
+	 * }
+	 * escribo.close();
+	 * }catch (IOException e) {
+	 * System.out.println("Ha ocurrido un error al crear el archivo.");
+	 * e.printStackTrace();
+	 * return false;
+	 * }
+	 * return true;
+	 * }
+	 */
 }
-
