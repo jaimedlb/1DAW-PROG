d8ded1a27e20bf2b633157c2bc1a60b531da1182
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-24 09:19:28 +0100

    Creacion de Clase Utilidades

---
+++ CalculadoraDeNota-jdominguez/src/main/java/com/gsd/daw/prog/Utilidades.java
@@ -0,0 +1,80 @@
+package com.gsd.daw.prog;
+
+public class Utilidades {
+	public static boolean robustez(String[] argumentos) {
+		boolean cierto = true;
+		int C = 0, P = 0, A = 0, T = 0;
+		if (argumentos.length == 0) {
+			System.out.println("ponme algun argumento");
+			return cierto = false;
+		}
+		if (argumentos.length > 20) {
+			System.out.println("no pongas mas de 20 argumentos");
+			return cierto = false;
+		} else {
+			for (int i = 0; i < argumentos.length; i++) {
+				String[] caracteres = (argumentos[i].split("-"));
+				if (caracteres.length == 1) {
+					System.out.println("pasame bien el formato [CPTA]-<numero-decimal>");
+					return cierto = false;
+				} else {
+					if ((caracteres[0].equals("C") || caracteres[0].equals("P") || caracteres[0].equals("A")
+							|| caracteres[0].equals("T"))) {
+
+						try {
+							Double.parseDouble(caracteres[1]);
+						} catch (NumberFormatException qwe) {
+
+							System.out.println("pasamed bien el formato [CPTA]-<numero-decimal>");
+							return cierto = false;
+						}
+					} else {
+						System.out.println("pasame bien el formato [CPTA]-<numero-decimal>");
+						return cierto = false;
+					}
+				}
+				if (cierto) {
+					if ((caracteres[0].equals("C"))) {
+						C++;
+						if (Double.parseDouble(caracteres[1]) < 0 || Double.parseDouble(caracteres[1]) > 10) {
+							System.out.println(
+									"ERROR: el numero en la nota [" + argumentos[i] + "] no esta dentro de limites");
+							return cierto = false;
+						}
+					} else {
+						if ((caracteres[0].equals("P"))) {
+							P++;
+							if (Double.parseDouble(caracteres[1]) < 0 || Double.parseDouble(caracteres[1]) > 10) {
+								System.out.println("ERROR: el numero en la nota [" + argumentos[i]
+										+ "] no esta dentro de limites");
+								return cierto = false;
+							}
+						} else {
+							if ((caracteres[0].equals("A"))) {
+								A++;
+								if (Double.parseDouble(caracteres[1]) < 0 || Double.parseDouble(caracteres[1]) > 1) {
+									System.out.println("ERROR: el numero en la nota [" + argumentos[i]
+											+ "] no esta dentro de limites");
+									return cierto = false;
+								}
+							} else {
+								T++;
+								if (Double.parseDouble(caracteres[1]) < 0 || Double.parseDouble(caracteres[1]) > 1) {
+									System.out.println("ERROR: el numero en la nota [" + argumentos[i]
+											+ "] no esta dentro de limites");
+									return cierto = false;
+								}
+							}
+						}
+					}
+				}
+			}
+			if (C >= 1 && P >= 1 && A == 1 && T == 1) {
+				return cierto;
+			} else {
+				System.out.println("tiene que haber al menos una nota de examen, practicas, asistencia y actitud");
+				return cierto = false;
+			}
+		}
+	}
+}
