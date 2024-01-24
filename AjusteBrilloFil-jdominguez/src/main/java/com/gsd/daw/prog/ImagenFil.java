b2a25610435a35a86e1998496c38281ee37c5ddf
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-24 13:47:40 +0100

    tus muertos

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/ImagenFil.java
@@ -8,6 +8,8 @@ public class ImagenFil {
 
 	public static String[] ConversorArray(String[] fil) {
 		Scanner inputFile = null;
+		int filas = 0;
+		int columnas = 0;
 		try {
 			File f = new File(fil[0]);
 			inputFile = new Scanner(f);
@@ -16,9 +18,17 @@ public class ImagenFil {
 			System.err.println("ERROR: no se puede abrir el fichero [" + fil[0] + "]");
 			return null;
 		}
+
 		String comentario = String.valueOf(inputFile.nextLine());
-		int filas = Integer.parseInt(inputFile.nextLine());
-		int columnas = Integer.parseInt(inputFile.nextLine());
+		try {
+			filas = Integer.parseInt(inputFile.nextLine());
+			columnas = Integer.parseInt(inputFile.nextLine());
+		} catch (Exception e) {
+			System.err.println("Las lineas 2 y 3 tiene que ser numero para saber la longitud");
+			inputFile.close();
+			return null;
+		}
+
 		String[] parametros = new String[(filas * columnas) + 3];
 		parametros[0] = comentario;
 		parametros[1] = String.valueOf(filas);
@@ -35,28 +45,16 @@ public class ImagenFil {
 	/*
 	 * public static boolean StringconversorFil(String[] fil) {
 	 * 
-	 * try {
-	 * String[] nombreSpliteado=String.valueOf(fil[0]).split("#|\\.");
-	 * String nombreArchivo=nombreSpliteado[1];
-	 * String nombreFinal=nombreArchivo+"-cambiado.fil";
-	 * File archivo =new File(nombreFinal);
+	 * try { String[] nombreSpliteado=String.valueOf(fil[0]).split("#|\\."); String
+	 * nombreArchivo=nombreSpliteado[1]; String
+	 * nombreFinal=nombreArchivo+"-cambiado.fil"; File archivo =new
+	 * File(nombreFinal);
 	 * 
 	 * FileWriter escribo = new FileWriter(archivo);
-	 * escribo.write("#"+nombreFinal+"\n");
-	 * for (int i = 1; i < fil.length; i++) {
-	 * if(i==fil.length-1) {
-	 * escribo.write(fil[i]);
-	 * }else {
-	 * escribo.write(fil[i]+"\n");
-	 * }
-	 * }
-	 * escribo.close();
-	 * }catch (IOException e) {
+	 * escribo.write("#"+nombreFinal+"\n"); for (int i = 1; i < fil.length; i++) {
+	 * if(i==fil.length-1) { escribo.write(fil[i]); }else {
+	 * escribo.write(fil[i]+"\n"); } } escribo.close(); }catch (IOException e) {
 	 * System.out.println("Ha ocurrido un error al crear el archivo.");
-	 * e.printStackTrace();
-	 * return false;
-	 * }
-	 * return true;
-	 * }
+	 * e.printStackTrace(); return false; } return true; }
 	 */
 }
--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/Utilidades.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/Utilidades.java
@@ -1,15 +1,22 @@
 package com.gsd.daw.prog;
-
+import java.io.File;
+import java.util.Scanner;
 public class Utilidades {
 	public static boolean ComprobarRobustez(String[] args) {
-		if(args.length!=2) {
+		if (args.length != 2) {
 			System.err.println("pasame dos argumentos");
 			return false;
 		}
-		if(Integer.parseInt(args[1])>255||Integer.parseInt(args[1])<-255) {
+		if (Integer.parseInt(args[1]) > 255 || Integer.parseInt(args[1]) < -255) {
 			System.err.println("pasame un numero entre -255 y 255");
 			return false;
 		}
+
+		return true;
+	}
+
+	public static boolean comprobarLineas (File archivo) {
+	
 		
 		return true;
 	}
