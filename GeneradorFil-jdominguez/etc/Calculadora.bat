10b95e117dc4504b9a74125b452156c309c77ee7
jaime <jaime>
2024-01-25 11:41:19 +0100

    GeneradorFil

---
+++ GeneradorFil-jdominguez/etc/Calculadora.bat
@@ -0,0 +1,7 @@
+java -jar ../target/ejemplo-1.0.jar C-7 C-8 P-5 P-6 A-0.7 T-0.8
+java -jar ../target/ejemplo-1.0.jar C-2 C-8 P-8 A-1 T-0.5
+java -jar ../target/ejemplo-1.0.jar C-9 C-9 P-2 P-10 A-0 T-1
+java -jar ../target/ejemplo-1.0.jar C-72 C-5 P-5 P-6 A-1 T-0
+java -jar ../target/ejemplo-1.0.jar C-10 C-10 P-10 P-10 A-1 T-1
+java -jar ../target/ejemplo-1.0.jar C-9 C-8 P-5 P-9 A-0.2 
+pause
