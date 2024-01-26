e32b4bcce08b98fa21e60c32be943c84d3fe7620
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-26 13:29:40 +0100

    cambios

--- GeneradorFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
+++ GeneradorFil-jdominguez/src/main/java/com/gsd/daw/prog/App.java
@@ -6,25 +6,11 @@ import java.io.FileWriter;
 public class App {
 
 	public static void main(String[] args) {
-		File archivo = new File("aleatorio.fil");
-		int ancho = Integer.parseInt(args[0]);
-		int alto = Integer.parseInt(args[1]);
-		FileWriter escribo = null;
-		try {
-			escribo = new FileWriter(archivo);
-			escribo.write("# archivo Generado aleatoriamente \n");
-			escribo.write(ancho+"\n");
-			escribo.write(alto+"\n");
-			for (int i = 0; i < (ancho * alto)-1; i++) {
-				escribo.write(GenereadorFil.GeneradorAleatorioRGB()+"\n");
-			}
-			escribo.write(GenereadorFil.GeneradorAleatorioRGB());
-
-			escribo.close();
-
-		} catch (Exception e) {
-			System.err.println("no se encuentra el archivo");
-			return;
+		System.out.println("#aleatorio");
+System.out.println(args[0]);
+System.out.println(args[1]);
+		for (int i = 0; i < (Integer.parseInt(args[0])*Integer.parseInt(args[1])); i++) {
+			System.out.println(GenereadorFil.GeneradorAleatorioRGB());
 		}
 		
 	}
