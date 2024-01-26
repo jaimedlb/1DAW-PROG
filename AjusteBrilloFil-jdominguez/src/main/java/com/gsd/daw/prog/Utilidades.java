e05fcc31c2808b69393ddff38a9455e9ee7ed3fc
j.dominguez <j.dominguez@2DAW-13.BUINF.local>
2024-01-26 09:51:31 +0100

    arreglo robustez RGB

--- AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/Utilidades.java
+++ AjusteBrilloFil-jdominguez/src/main/java/com/gsd/daw/prog/Utilidades.java
@@ -9,6 +9,16 @@ public class Utilidades {
 		if (args.length != 2) {
 			System.err.println("pasame dos argumentos");
 			return false;
+		}try {
+			Integer.parseInt(args[1]);
+			
+		} catch (Exception e) {
+			System.out.println("El formato de RGB tiene que ser 0-255,0-255,0-255");
+			return false;
+		}
+		if (Integer.parseInt(args[1]) > 255 || Integer.parseInt(args[1]) < -255) {
+			System.err.println("pasame un numero entre -255 y 255");
+			return false;
 		}
 		if (Integer.parseInt(args[1]) > 255 || Integer.parseInt(args[1]) < -255) {
 			System.err.println("pasame un numero entre -255 y 255");
