a217ea89d4b68017876f72228b276211041b98cd
jaime <jaime@php>
2024-02-28 09:11:15 +0100

    constructor

--- ApacheLogDataMining-jdominguez/src/main/java/com/gsd/daw/prog/ApacheLogLoader/Log.java
+++ ApacheLogDataMining-jdominguez/src/main/java/com/gsd/daw/prog/ApacheLogLoader/Log.java
@@ -7,6 +7,15 @@ private String request;
 private String result;
 private String bytes;
 private String ua;
+public Log(String ip, String timeStamp, String request, String result, String bytes, String ua) {
+	super();
+	this.ip = ip;
+	this.timeStamp = timeStamp;
+	this.request = request;
+	this.result = result;
+	this.bytes = bytes;
+	this.ua = ua;
+}
 
 
 }
