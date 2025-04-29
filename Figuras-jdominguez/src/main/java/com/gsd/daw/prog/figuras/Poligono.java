52820bc26f6f34a7045d669f271b94ffa91858e4
jaime <youremail@yourdomain.com>
2025-04-29 13:39:00 +0200

    Poligono

--- Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Poligono.java
+++ Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Poligono.java
@@ -1,5 +1,37 @@
 package com.gsd.daw.prog.figuras;
 
 public class Poligono {
+private Punto[] puntos;
+private Stroke stroke;
+
+
+public Poligono(Punto[] puntos) {
+	if (puntos == null || puntos.length == 0)
+		throw new IllegalArgumentException();
+	
+	this.puntos = puntos;
+	this.stroke = new Stroke(new Color((byte) 0, (byte) 0, (byte) 0), 1);
+}
+
+public void setStroke(Stroke stroke) {
+	if (stroke == null)
+		throw new IllegalArgumentException();
+	this.stroke = stroke;
+}
+
+public String toSvg() {
+	
+	StringBuilder sb=new StringBuilder();
+	
+	sb.append("<polygon points=\"");
+	for(Punto punto:puntos) {
+		sb.append(punto.getX()).append(",").append(punto.getY()).append(" ");
+	}
+	if(sb.charAt(sb.length()-1)==' ') {
+		sb.setLength(sb.length()-1);
+	}
+	sb.append("\" ").append(stroke.toSvg()).append(" fill=\"none\"/>");
+	return sb.toString();
+}
 
 }
