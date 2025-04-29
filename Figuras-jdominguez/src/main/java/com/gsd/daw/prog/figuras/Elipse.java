2f3d9ef7c811c4459c16093ea0ebe4e1f6c6781b
jaime <youremail@yourdomain.com>
2025-04-29 13:11:26 +0200

    Linea

--- Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Elipse.java
+++ Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Elipse.java
@@ -15,6 +15,8 @@ public class Elipse {
 		this.stroke=new Stroke(new Color((byte) 0, (byte) 0, (byte) 0), 1);
 	}
 	public void setStroke(Stroke stroke) {
+		if (stroke == null)
+			throw new IllegalArgumentException();
 		this.stroke=stroke;
 	}
 	public String toSvg() {
--- Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Linea.java
+++ Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Linea.java
@@ -1,5 +1,29 @@
 package com.gsd.daw.prog.figuras;
 
 public class Linea {
+private Punto punto1;
+private Punto punto2;
+private Stroke stroke;
+public Linea(Punto punto1, Punto punto2) {	
+	if(punto1==null||punto2==null)
+		throw new IllegalAccessError();
+	
+	this.punto1=punto1;
+	this.punto2=punto2;
+	this.stroke=new Stroke(new Color((byte) 0, (byte) 0, (byte) 0), 1);
+}
 
+public void setStroke(Stroke stroke) {
+	if (stroke == null)
+		throw new IllegalArgumentException();
+	this.stroke=stroke;
+}
+
+public String toSvg() {
+	StringBuilder sb= new StringBuilder();	
+	sb.append("<line x1=\"").append(punto1.getX()).append("\" y1=\"").append(punto1.getY())
+	.append("\" x2=\"").append(punto2.getX()).append("\" y2=\"").append(punto2.getY()).append("\" ").append(stroke.toSvg()).append(" fill=\"none\"/>");
+	
+	return sb.toString();
+}
 }
