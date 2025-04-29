e220762a6f5ed8e8c97b43eae29557d8e2823d58
jaime <youremail@yourdomain.com>
2025-04-29 15:34:02 +0200

    Cambio en el color, Contenedor

--- Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Color.java
+++ Figuras-jdominguez/src/main/java/com/gsd/daw/prog/figuras/Color.java
@@ -2,17 +2,17 @@ package com.gsd.daw.prog.figuras;
 
 public class Color {
 
-	private Byte red;
-	private Byte green;
-	private Byte blue;
+	private final int red;
+	private final int green;
+	private final int blue;
 
 	public Color(Byte red, Byte green, Byte blue) {
 
 		if (red == null || green == null || blue == null)
 			throw new IllegalArgumentException();
-		this.red = red;
-		this.green = green;
-		this.blue = blue;
+		this.red = red & 0xFF;
+		this.green = green & 0xFF;
+		this.blue = blue & 0xFF;
 
 	}
 
